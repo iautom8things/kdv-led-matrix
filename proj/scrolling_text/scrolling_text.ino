a875aa2b68870a21bc6cc7b2aff3e9e9b41924cc
Manuel Zubieta <mazubieta@gmail.com>
2016-01-19 23:33:25 -0600

    add some quick comments

--- proj/scrolling_text/scrolling_text.ino
+++ proj/scrolling_text/scrolling_text.ino
@@ -12,9 +12,11 @@ vector<Column*> columns;
 void setup() {
   FastLED.addLeds<WS2811, PIN_OUT>(leds, NUM_LEDS);
 
+  // create vector of columns
   for(int c = 0; c < WIDTH; c++) {
     vector<CRGB*> *column = new vector<CRGB*>();
     for(int r = 0; r < HEIGHT; r++) {
+      // handle snaking leds
       if (r % 2 != 0) {
         column->push_back(&(leds[(1+r)*WIDTH-(c+1)]));
       } else {
@@ -23,36 +25,45 @@ void setup() {
     }
     columns.push_back(new Column(column));
   }
+
+  // seed some initial colors on the far right
   leds[19] = CRGB::Red;
   leds[20] = CRGB::Blue;
   leds[59] = CRGB::Violet;
   leds[60] = CRGB::Green;
+  leds[99] = CRGB::Brown;
+  leds[100]= CRGB::Orange;
 }
 
 void loop() {
 
-    for(int right = WIDTH-1; right > 0; right--) {
-      int left = (WIDTH+right-1) % WIDTH;
-      vector<CRGB*> from = columns[right]->getPixels();
-      vector<CRGB*>   to = columns[left]->getPixels();
-      for(int i = 0; i < HEIGHT; i++) {
-        *to[i] = *from[i];
-      }
-      FastLED.show();
-      columns[right]->clear();
-      FastLED.show();
-      delay(100);
+  // FIXME: there's something wrong with my edge case that causes it not to cycle
+  // go from right to left copying all of one column to the next
+  for(int right = WIDTH-1; right > 0; right--) {
+    int left = (WIDTH+right-1) % WIDTH;
+    vector<CRGB*> from = columns[right]->getPixels();
+    vector<CRGB*>   to = columns[left]->getPixels();
+    for(int i = 0; i < HEIGHT; i++) {
+      *to[i] = *from[i];
     }
-      int left = 19;
-      int right = 0;
-      vector<CRGB*> from = columns[right]->getPixels();
-      vector<CRGB*>   to = columns[left]->getPixels();
-      for(int i = 0; i < HEIGHT; i++) {
-        *to[i] = *from[i];
-      }
-      FastLED.show();
-      columns[right]->clear();
-      FastLED.show();
-      delay(100);
-    
+    FastLED.show();
+    columns[right]->clear();
+    FastLED.show();
+    delay(100);
+  }
+
+  // TODO: remove me
+  // since above doesn't restart, force a restart by copying far left column to far right
+  int left = 19;
+  int right = 0;
+  vector<CRGB*> from = columns[right]->getPixels();
+  vector<CRGB*>   to = columns[left]->getPixels();
+  for(int i = 0; i < HEIGHT; i++) {
+    *to[i] = *from[i];
+  }
+  FastLED.show();
+  columns[right]->clear();
+  FastLED.show();
+  delay(100);
+
 }
